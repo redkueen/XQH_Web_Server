81567445d4a4fa676a5743a284eeb1ff47c9f468
redkueen <xqh20186@163.com>
2017-07-31 17:13:52 +0800

    新启动的线程直接读取客户端的数据。

--- src/XQHRunnable.java
+++ src/XQHRunnable.java
@@ -9,8 +9,18 @@ public class XQHRunnable implements Runnable{
 
     @Override
     public void run() {
+
         try {
-            BufferedReader BR = new BufferedReader(new FileReader("D:\\XQH_Web_Server\\src\\goods.html"));
+//            BufferedReader bis = new BufferedReader( client.getInputStream());
+//            byte[] b= new byte[bis.available()];
+//            bis.read(b);
+//            String s=new String(b);
+
+            BufferedReader br2 = new BufferedReader( new InputStreamReader(client.getInputStream()));
+            String L=br2.readLine();
+            String f = L.split(" ")[1];
+            BufferedReader BR = new BufferedReader(new FileReader("D:\\XQH_Web_Server\\src\\" + f));
+//            BufferedReader BR = new BufferedReader(new FileReader("D:\\XQH_Web_Server\\src\\goods.html"));
             String k = null;
             String n ="";
             while ((k = BR.readLine()) != null) {
@@ -18,11 +28,12 @@ public class XQHRunnable implements Runnable{
             }
             BufferedWriter BW = new BufferedWriter(new OutputStreamWriter(client.getOutputStream()));
             BW.write("HTTP/1.1 200 OK\n" +
-                    "Content-Length:" + n.length() + "\n" +
+                    "Content-Length:" + n.getBytes().length + "\n" +
                     "\n" +
                     n
             );
             BW.flush();
+            br2.close();
             client.close();
             BR.close();
             BW.close();
